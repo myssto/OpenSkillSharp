05b11e9a7bb9d769fdce50b33c1a729cc87f16e7
myssto <pat.lacni@yahoo.com>
2025-08-08 20:10:11 -0500

    better statistics docs

--- OpenSkillSharp/Util/Statistics.cs
+++ OpenSkillSharp/Util/Statistics.cs
@@ -5,37 +5,58 @@ using MathNet.Numerics.Distributions;
 namespace OpenSkillSharp.Util;
 
 /// <summary>
-/// Utility for calculating values of a normal distribution.
+/// Provides statistical utilities for solving values based on the standard normal distribution.
 /// </summary>
 public static class Statistics
 {
     private static readonly Normal Normal = new(0, 1);
     private const double Epsilon = 1e-10;
-
+    
     /// <summary>
-    /// Normal cumulative distribution.
+    /// Computes the cumulative distribution (CDF) of the standard normal distribution at the given number.
     /// </summary>
+    /// <param name="x">A number representing the location for which to evaluate the CDF.</param>
+    /// <returns>
+    /// The probability that a random variable from the standard normal distribution is less than or equal to
+    /// <paramref name="x"/> represented as a number between 0 and 1.
+    /// </returns>
     public static double PhiMajor(double x)
     {
         return Normal.CumulativeDistribution(x);
     }
 
     /// <summary>
-    /// Normal inverse cumulative distribution.
+    /// Computes the inverse of the cumulative distribution function (InvCDF) for the standard normal distribution at
+    /// the given probability.
     /// </summary>
+    /// <param name="x">A number representing the location for which to evaluate the InvCDF.</param>
+    /// <returns>The inverse cumulative density at <paramref name="x"/>.</returns>
     public static double InversePhiMajor(double x)
     {
         return Normal.InverseCumulativeDistribution(x);
     }
-
+    
     /// <summary>
-    /// Normal probability density.
+    /// Computes the probability density of the standard normal distribution (PDF) at the given number.
     /// </summary>
+    /// <param name="x">A number representing the location at which to compute the density.</param>
+    /// <returns>The probability density at <paramref name="x"/>.</returns>
     public static double PhiMinor(double x)
     {
         return Normal.Density(x);
     }
 
+    /// <summary>
+    /// The function <c>V</c> as defined in
+    /// <a href="https://jmlr.org/papers/v12/weng11a.html"><c>JMLR:v12:weng11a</c></a>.
+    /// <br/>
+    /// Represented by:
+    /// <br/>
+    /// <c>V (x,t) = φ(x − t)/Φ(x − t)</c>
+    /// </summary>
+    /// <param name="x">A number.</param>
+    /// <param name="t">A number.</param>
+    /// <returns>A number.</returns>
     public static double V(double x, double t)
     {
         double xt = x - t;
@@ -46,6 +67,17 @@ public static class Statistics
             : PhiMinor(xt) / denominator;
     }
 
+    /// <summary>
+    /// The function <c>W</c> as defined in
+    /// <a href="https://jmlr.org/papers/v12/weng11a.html"><c>JMLR:v12:weng11a</c></a>.
+    /// <br/>
+    /// Represented by:
+    /// <br/>
+    /// <c>W (x,t) = V (x,t)(V (x,t) + (x − t))</c>
+    /// </summary>
+    /// <param name="x">A number.</param>
+    /// <param name="t">A number.</param>
+    /// <returns>A number.</returns>
     public static double W(double x, double t)
     {
         double xt = x - t;
@@ -59,6 +91,17 @@ public static class Statistics
         return V(x, t) * (V(x, t) + xt);
     }
 
+    /// <summary>
+    /// The function <c>~V</c> as defined in
+    /// <a href="https://jmlr.org/papers/v12/weng11a.html"><c>JMLR:v12:weng11a</c></a>.
+    /// <br/>
+    /// Represented by:
+    /// <br/>
+    /// <c>~V (x,t) = −((φ(t − x) − φ(−t − x)) / (Φ(t − x) − Φ(−t − x)))</c>
+    /// </summary>
+    /// <param name="x">A number.</param>
+    /// <param name="t">A number.</param>
+    /// <returns>A number.</returns>
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public static double VT(double x, double t)
     {
@@ -76,6 +119,17 @@ public static class Statistics
         return (x < 0 ? -a : a) / b;
     }
 
+    /// <summary>
+    /// The function <c>~W</c> as defined in
+    /// <a href="https://jmlr.org/papers/v12/weng11a.html"><c>JMLR:v12:weng11a</c></a>.
+    /// <br/>
+    /// Represented by:
+    /// <br/>
+    /// <c>~W (x,t) = (((t − x)φ(t − x) − (−(t + x))φ(−(t + x))) /  (Φ(t − x) − Φ(−t − x))) + ~V(x,t)^2</c>
+    /// </summary>
+    /// <param name="x">A number.</param>
+    /// <param name="t">A number.</param>
+    /// <returns>A number.</returns>
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public static double WT(double x, double t)
     {
